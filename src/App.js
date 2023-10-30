0a25e9bd456b27ebce5a5663eebe6566b8dcb3e9
Tomás Dias <tomas.dias@appx.pt>
2023-10-30 15:52:14 +0000

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import chambersRaw from "./resources/chambers.json"
+import Chamber from "./components/chamber";
+import {useState} from "react";
+import lupa from "./resources/img/lupa.png"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [chambers, setChambers] = useState(chambersRaw);
+    const [TextValue, setTextValue] = useState("");
+
+    function searchChamber() {
+        if (chambers === "chambers")
+            setChambers(chambersRaw)
+        else {
+            setChambers(chambersRaw.filter((chamber) => {
+                return chamber.name.toLowerCase().includes(TextValue.toLowerCase().trim())
+            }))
+        }
+    }
+
+    return (<>
+            <div>
+                <div className={"header"}>
+                    <div className={"title"}>
+                        <p>My Chambers</p>
+                    </div>
+                    <div className={"searchBar"}>
+                        <img onClick={searchChamber} src={lupa} height={15} width={15} alt={"lupa"}/>
+                        <input className={"hiddenInput"} type={"text"} placeholder={"Search"} value={TextValue}
+                               onKeyDown={(e) => {
+                                   if (e.key === "Enter") {
+                                       searchChamber()
+                                   }
+                               }
+                               }
+                               onChange={(e) => setTextValue(e.target.value)}/>
+                    </div>
+                </div>
+                <div className={"content"}>
+                    <div className={"chamberList"}>
+                        {chambers.map((chamber, index) =>
+                            <Chamber chamber={chamber} key={index}/>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
